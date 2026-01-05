92c2b618a158a4280b9b45e67b27a917e72f1a46
Karl <karl.vareskog@icloud.com>
2026-01-05 14:23:18 +0100

    override NODE_PATH in startup

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# Custom startup script to bypass Azure Oryx's node_modules extraction
+# Override NODE_PATH to use our app's node_modules, not Azure's cached /node_modules
+export NODE_PATH=/home/site/wwwroot/node_modules
+export PATH=/home/site/wwwroot/node_modules/.bin:$PATH
 cd /home/site/wwwroot
 node server.js
